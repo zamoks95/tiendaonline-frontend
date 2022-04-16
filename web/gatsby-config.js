a1d191b3ee7bf1c4181c8a848226bfce39b7bdbc
Sergi Zamora Montesinos <sergi.zamora.95@gmail.com>
2022-04-16 17:29:58 +0200

    a

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -36,12 +36,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GTAG,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
